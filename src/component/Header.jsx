aa80cebceb88ec303e1d739123128771a56fff6c
husanjonmusayev <husanjonm279@gmail.com>
2024-01-28 11:29:10 +0500

    uchunchi dars

---
+++ src/component/Header.jsx
@@ -0,0 +1,32 @@
+export default function Header() {
+  return (
+    <header>
+      <a href="#">
+        <img src="/public/Logo.png" alt="web_site_logo" />
+      </a>
+
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About us</a>
+          </li>
+          <li>
+            <a href="#">How it works</a>
+          </li>
+          <li>
+            <a href="#">Services</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+        <a className="login" href="#">
+          Login
+        </a>
+      </nav>
+    </header>
+  );
+}
---
+++ src/component/Hero.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function Hero() {
+  return (
+    <div className="hero">
+      <div className="texSection">
+        <div className="text">
+          <h2>
+            Easy <br /> payment for any business
+          </h2>
+        </div>
+        <p>
+          For ambitious companies around the world, wallet makes moving money as
+          simple, borderless and programmable as the rest of the internet.
+        </p>
+        <a className="startBtn" href="">
+          Get Started
+        </a>
+      </div>
+      <div className="imgSection">
+        <img src="/public/Group 476.png" alt="hero_img" />
+      </div>
+    </div>
+  );
+}
